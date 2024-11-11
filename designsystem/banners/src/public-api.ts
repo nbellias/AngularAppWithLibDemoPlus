67e1c7114b244c1800605effcfed7c5f0889fc00
Nikolaos Bellias <nbellias@nibelsoft.com>
2024-11-11 14:55:34 +0100

    Initial commit

---
+++ designsystem/banners/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of banners
+ */
+
+export * from './lib/banners.service';
+export * from './lib/banners.component';
---
+++ designsystem/buttons/src/lib/special-button/special-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-special-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './special-button.component.html',
+  styleUrl: './special-button.component.css'
+})
+export class SpecialButtonComponent {
+
+}
